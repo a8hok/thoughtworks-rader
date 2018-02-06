528b2d26b6f7fdfbadf393215684298c6139ab71
ntsadmin <ntsadmin@nts203703mc.local>
2018-02-06 19:11:34 +0530

    Color coding for items

--- src/util/factory.js
+++ src/util/factory.js
@@ -39,7 +39,7 @@ const plotRadar = function (title, blips) {
         if (!quadrants[blip.quadrant]) {
             quadrants[blip.quadrant] = new Quadrant(_.capitalize(blip.quadrant));
         }
-        quadrants[blip.quadrant].add(new Blip(blip.name, ringMap[blip.ring], blip.isNew.toLowerCase() === 'true', blip.topic, blip.description))
+        quadrants[blip.quadrant].add(new Blip(blip.name, ringMap[blip.ring], blip.isNew.toLowerCase() === 'true', blip.topic, blip.description, blip.colour))
     });
 
     var radar = new Radar();
